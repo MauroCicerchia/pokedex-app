1673c8079c4c7a14531903b6ec42926ca52f9277
MauroCicerchia <maurocicerchia98@gmail.com>
2021-02-15 13:26:36 -0300

    feat: add highlight to cards

--- src/components/BaseCard/index.tsx
+++ src/components/BaseCard/index.tsx
@@ -1,19 +1,38 @@
 import React, { useContext } from "react";
-import { View, Text, Image } from "react-native";
+import {
+	View,
+	Text,
+	Image,
+	TouchableHighlight,
+	TouchableNativeFeedback,
+	Platform,
+} from "react-native";
 import { ThemeContext } from "react-native-elements";
 import useStyles from "./styles";
 import IGenerationData from "../../interfaces/IGenerationData";
 
-export default function BaseCard({ image, generation }: IGenerationData	) {
+export default function BaseCard({ image, generation }: IGenerationData) {
 	const { theme } = useContext(ThemeContext);
 	const styles = useStyles(theme);
-	const backgroundSource = theme.dark ? require("../../images/pokeball-dark.png") : require("../../images/pokeball-light.png");
+	const backgroundSource = theme.dark
+		? require("../../images/pokeball-dark.png")
+		: require("../../images/pokeball-light.png");
 
 	return (
-		<View style={styles.container}>
-			<Image style={styles.imageBackground} source={backgroundSource} />
-			<Text style={styles.text}>Generación {generation}</Text>
-			<Image style={styles.image} resizeMode="center" source={image} />
-		</View>
+		<NativeFeedback>
+			<View style={styles.container}>
+				<Image style={styles.imageBackground} source={backgroundSource} />
+				<Text style={styles.text}>Generación {generation}</Text>
+				<Image style={styles.image} resizeMode="center" source={image} />
+			</View>
+		</NativeFeedback>
+	);
+}
+
+function NativeFeedback({ children }) {
+	return Platform.OS === "android" ? (
+		<TouchableNativeFeedback>{children}</TouchableNativeFeedback>
+	) : (
+		<TouchableHighlight>{children}</TouchableHighlight>
 	);
 }
