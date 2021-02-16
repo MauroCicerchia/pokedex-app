b72f838a603ba3e7f379527341719a494d61950a
Kevin Szuchet <kevinszuchet@gmail.com>
2021-02-16 08:25:41 -0300

    use the navigation container as a theme provider

--- App.tsx
+++ App.tsx
@@ -3,11 +3,9 @@ import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useColorScheme } from "react-native-appearance";
-import { ThemeProvider } from "react-native-elements";
 import themes from "./src/themes";
 import HomeScreen from "./src/views/Home";
 import GenerationDetailScreen from "./src/views/GenerationDetail";
-import IGenerationData from "./src/interfaces/IGenerationData";
 
 const Stack = createStackNavigator();
 
@@ -19,26 +17,24 @@ export default function App() {
 	const theme = themes.dark;
 
 	return (
-		<NavigationContainer>
-			<ThemeProvider theme={theme}>
-				<Stack.Navigator 
-					screenOptions={{
-						headerStyle: {
-							backgroundColor: '#f4511e',
-						},
-						headerTintColor: '#fff',
-						headerTitleStyle: {
-							fontWeight: 'bold',
-						},
-					}}>
-					<Stack.Screen name="Pokedex" component={HomeScreen} />
-					<Stack.Screen
-						name="Generation"
-						component={GenerationDetailScreen}
-						options={generationTitle}
-					/>
-				</Stack.Navigator>
-			</ThemeProvider>
+		<NavigationContainer theme={theme}>
+			<Stack.Navigator 
+				screenOptions={{
+					headerStyle: {
+						backgroundColor: '#f4511e',
+					},
+					headerTintColor: '#fff',
+					headerTitleStyle: {
+						fontWeight: 'bold',
+					},
+				}}>
+				<Stack.Screen name="Pokedex" component={HomeScreen} />
+				<Stack.Screen
+					name="Generation"
+					component={GenerationDetailScreen}
+					options={generationTitle}
+				/>
+			</Stack.Navigator>
 		</NavigationContainer>
 	);
 }
